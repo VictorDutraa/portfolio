dd66696abf72c3df59d5202a77d6e666f1190b87
VictorDutraa <149853348+VictorDutraa@users.noreply.github.com>
2024-04-22 13:56:40 -0300

    ajeitando hover e touchend

--- script.js
+++ script.js
@@ -1,38 +1,46 @@
-if (window.matchMedia("(max-width: 1000px)").matches) {
-    var isTouched = false;
+var isTouching = false;
 
+if (window.matchMedia("(max-width: 1000px)").matches) {
     document.addEventListener('touchstart', function() {
-        isTouched = true;
-        document.querySelector('.frase-titulo').classList.add('hovered');
+        isTouching = true;
+        document.querySelector('.as-vezes').style.display = 'none';
+        document.querySelector('.sempre').style.display = 'block';
     });
 
     document.addEventListener('touchend', function() {
-        isTouched = false;
-        document.querySelector('.frase-titulo').classList.remove('hovered');
-    });
-
-    document.querySelector('.frase-titulo').addEventListener('touchstart', function() {
-        isTouched = true;
-        document.querySelector('.frase-titulo').classList.remove('hovered');
-    });
-
-    document.querySelector('.frase-titulo').addEventListener('touchend', function() {
-        isTouched = false;
-        document.querySelector('.frase-titulo').classList.add('hovered');
-    });
-
-    document.querySelector('.frase-titulo').addEventListener('mouseover', function() {
-        if (!isTouched) {
-            document.querySelector('.frase-titulo').classList.remove('hovered');
-        }
-    });
-
-    document.querySelector('.frase-titulo').addEventListener('mouseleave', function() {
-        if (!isTouched) {
-            document.querySelector('.frase-titulo').classList.add('hovered');
-        }
+        isTouching = false;
+        document.querySelector('.as-vezes').style.display = 'block';
+        document.querySelector('.sempre').style.display = 'none';
     });
 }
+
+document.querySelector('.sobre-mim-longo').addEventListener('mouseover', function() {
+    if (!isTouching) {
+        document.querySelector('.as-vezes').style.display = 'none';
+        document.querySelector('.sempre').style.display = 'block';
+    }
+});
+
+document.querySelector('.sobre-mim-longo').addEventListener('mouseleave', function() {
+    if (!isTouching) {
+        document.querySelector('.as-vezes').style.display = 'block';
+        document.querySelector('.sempre').style.display = 'none';
+    }
+});
+
+document.querySelector('.sobre-mim-longo').addEventListener('mouseenter', function() {
+    if (!isTouching) {
+        document.querySelector('.as-vezes').style.display = 'none';
+        document.querySelector('.sempre').style.display = 'block';
+    }
+});
+
+document.querySelector('.sobre-mim-longo').addEventListener('mouseleave', function() {
+    if (!isTouching) {
+        document.querySelector('.as-vezes').style.display = 'block';
+        document.querySelector('.sempre').style.display = 'none';
+    }
+});
 
 document.getElementById('btnEnviarEmail').addEventListener('click', function() {
     window.open('https://mail.google.com/mail/?view=cm&fs=1&tf=1&source=mailto&to=victor.d.jesus@edu.ufes.br');
